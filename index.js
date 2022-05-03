49b4c4391928c548da8c055cd4ff451b1f15be4e
zahidmiran <zahidmiron599@gmail.com>
2022-05-03 17:40:14 +0600

    mdb add

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 
+
+/*  */
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.1ktnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello Worlds!')
   })
