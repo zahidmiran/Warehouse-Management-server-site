2df9f98722d3b1cc9aed0ca35040180a7ccb8c3e
zahidmiran <zahidmiron599@gmail.com>
2022-05-07 14:39:38 +0600

    afsd

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
 const jwt = require('jsonwebtoken');
+
 require('dotenv').config()
 
 
 const port = process.env.PORT || 5000;
+app.use(cors())
+
 app.use(express.json())
 
 
