cb8b8e3dd1f5dcadd16a143fa337d440e71660f3
zahidmiran <zahidmiron599@gmail.com>
2022-05-07 14:29:22 +0600

    cors disable

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken');
-const cors = require('cors')
 require('dotenv').config()
 
 
 const port = process.env.PORT || 5000;
-app.use(cors())
 app.use(express.json())
 
 
